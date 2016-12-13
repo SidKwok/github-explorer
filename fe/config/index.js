8f38cdbf5e48a32cb86f31804f9b6d50314481ab
SidKwok <oceankwok@hotmail.com>
2016-12-13 11:08:51 +0800

    deploy on leancloud

--- fe/config/index.js
+++ fe/config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../../public/index.html'),
+    assetsRoot: path.resolve(__dirname, '../../public'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
